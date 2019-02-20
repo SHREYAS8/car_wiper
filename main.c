2f1b41ba84579d5210fed3d6fdf4650a0c225a44
SHREYAS8 <47587118+SHREYAS8@users.noreply.github.com>
2019-02-20 13:59:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <conio.h>
 
-/*int eng();
+int eng();
 void wiper();
 
 int main()
@@ -24,10 +23,4 @@ int main()
       printf("\nSystem is OFF\n");
  }
     return 0;
-}*/
-
-int main()
-{
- printf("hello world");
- return 0;
 }
